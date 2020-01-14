fd25ec3ea77772b221e20ee05bea027ac637748c
Murathan Akdemir <makdemir@ekin.com>
2020-01-14 13:24:19 +0300

    Data Preprocessing is completed.

---
+++ categorical_data.py
@@ -0,0 +1,28 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy= "mean"  ,verbose = 0)
+X[:, 1:3] = imputer.fit_transform(X[:, 1:3])
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+# labelencoder_X = LabelEncoder()
+# X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+columntransformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [0])],remainder='passthrough')
+X = np.array(columntransformer.fit_transform(X), dtype = np.float)
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
